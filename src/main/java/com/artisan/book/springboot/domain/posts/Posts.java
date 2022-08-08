b4b740eb0e07608a649c34f33a89511e6bd72d35
JaehongDev <println@kakao.com>
2022-08-08 20:39:26 +0900

    spring boot book chapter5

--- src/main/java/com/artisan/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/artisan/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.artisan.book.springboot.domain.posts;
 
+import com.artisan.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
